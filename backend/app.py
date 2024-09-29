27fc2098027a30f0033c3f358147035e951694aa
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2024-09-29 10:28:11 +0100

    All done for now.

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
@@ -14,10 +14,21 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL") or 'sqlit
 
 db = SQLAlchemy(app)
 
-with app.app_context():
-    db.create_all()
+# Serve the static folder (the distr) you got after running the npm run build
+frontend_folder = os.path.join(os.getcwd(), "..", "frontend", "distr")
+@app.route("/", defaults={"filename": ""})
+@app.route("/<path:filename>")
+def index(filename):
+    """Deploys the static folder"""
+    if not filename:
+        filename = "index.html"
+    return send_from_directory(frontend_folder, filename)
+
 
 import routes
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run()
